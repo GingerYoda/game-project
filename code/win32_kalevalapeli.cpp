146c4b664cefa28b0fd8bc0759e685619d5fcdb4
Oskari <oskari.perikangas@gmail.com>
2022-05-22 23:46:17 +0300

    Add windows programming enviroment

---
+++ code/win32_kalevalapeli.cpp
@@ -0,0 +1,14 @@
+/*------------------------------------------------------------------------------
+Oskari Perikangas
+oskari.perikangas@gmail.com
+ ------------------------------------------------------------------------------*/
+#include <windows.h>
+
+int __clrcall
+WinMain(HINSTANCE hInstance,
+		HINSTANCE hPrevInstance,
+		LPSTR     lpCmdLine,
+		int       nShowCmd)
+{
+  return 0;
+}
--- utilities/init.el
+++ utilities/init.el
@@ -1,11 +1,10 @@
 (setq inhibit-startup-message t) ; removes welcome buffer
 
 (scroll-bar-mode -1) ; Disable visible scrollbar
-(tool-bar-mode -1)   ; Disable the toolbar
+; (tool-bar-mode -1)   ; Disable the toolbar
 (tooltip-mode -1)    ; Disable tooltips
-(set-fringe-mode 10) ; Give some breathing space
-
-(menu-bar-mode -1)   ; Disable the menu bar
+(set-fringe-mode 10)
+;(menu-bar-mode -1)   ; Disable the menu bar
 
 (setq visible-bell t) ; Disables bell sound, adds screen flash
 
@@ -18,13 +17,22 @@
 (global-hl-line-mode) ; Highlights current line in buffer
 
 (set-frame-parameter (selected-frame) 'fullscreen 'maximized) ; Defaults fullscreen in start-up
+(set-cursor-color "blue")
+(blink-cursor-mode -1) ;Cursor doesn't blink anymore.
 
 ; Adds line and column numbers
-(column-number-mode)
-(display-line-numbers-mode)
+(line-number-mode 1)
+(global-display-line-numbers-mode 1)
+(column-number-mode 1)
 
 ; Sets tab size
 (setq-default tab-width 4)
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+; Sets different backup directory
+(setq backup-directory-alist `(("." . "~/.saves")))
+
+;Set default folder mainly for file visit purposes
+(setq default-directory "c:/Projects/")
